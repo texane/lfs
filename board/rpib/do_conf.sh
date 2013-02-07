6ee2395970b53314523ceb5479f5b6b2287a4529
Fabien Le Mentec <texane@gmail.com>
2013-02-07 15:08:55 -0600

    [ update, add ] board/common

--- board/rpib/do_conf.sh
+++ board/rpib/do_conf.sh
@@ -1,8 +1,7 @@
-export LFS_TARGET_ARCH=armv6
+export LFS_TARGET_ARCH=arm
 export LFS_CROSS_COMPILE=$LFS_CROSS_COMPILE_armv6
-export LFS_CTNG_VERS=1.18.0
-export LFS_DROPBEAR_VERS=2012.55
+
 export LFS_LINUX_VERS=3.6.1
 export LFS_LINUX_ARCH=arm
-export LFS_BUSYBOX_VERS=1.21.0
-export LFS_ZLIB_VERS=1.2.7
+
+. $LFS_TOP_DIR/board/common/do_conf.sh
