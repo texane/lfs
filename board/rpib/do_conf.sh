e111522ab9bb55081b1bd25cfa46dde5f05499f8
Fabien Le Mentec <texane@gmail.com>
2013-02-07 17:49:16 -0600

    [ update ] rpib board LINUX version

--- board/rpib/do_conf.sh
+++ board/rpib/do_conf.sh
@@ -1,7 +1,7 @@
 export LFS_TARGET_ARCH=arm
 export LFS_CROSS_COMPILE=$LFS_CROSS_COMPILE_armv6
 
-export LFS_LINUX_VERS=3.6.1
+export LFS_LINUX_VERS=3.6.y
 export LFS_LINUX_ARCH=arm
 
 . $LFS_TOP_DIR/board/common/do_conf.sh
