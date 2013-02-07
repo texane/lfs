40f2a3de46435d3a8625366812635e4e121fe54c
Fabien Le Mentec <texane@gmail.com>
2013-02-07 13:35:24 -0600

    [ update ] LFS_CROSS_COMPILE set by board

--- sh/do_user_globals.sh
+++ sh/do_user_globals.sh
@@ -5,10 +5,13 @@ case "$HOSTNAME" in
   export ftp_proxy=ftp://proxy.esrf.fr:3128
   export all_proxy=socks://proxy.esrf.fr:3128
   export https_proxy=https://proxy.esrf.fr:3128
-  export LFS_CROSS_COMPILE=/home/lementec/segfs/repo/dance/local/cross/.build/i686-nptl-linux-gnu/bin/i686-nptl-linux-gnu-
+  # toolchains
+  export LFS_CROSS_COMPILE_x86=/home/lementec/segfs/repo/dance/local/cross/.build/i686-nptl-linux-gnu/bin/i686-nptl-linux-gnu-
+  export LFS_CROSS_COMPILE_armv6=''
   ;;
  'dell')
-  export LFS_CROSS_COMPILE=''
+  export LFS_CROSS_COMPILE_x86=''
+  export LFS_CROSS_COMPILE_armv6=/home/texane/x-tools/armv6-rpi-linux-gnueabi/bin/armv6-rpi-linux-gnueabi-
   ;;
 esac
 
