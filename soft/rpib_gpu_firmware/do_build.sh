6c95ac025681a1717e4b5b06856bcc3fe6f54599
Fabien Le Mentec <texane@gmail.com>
2013-02-07 16:31:58 -0600

    [ fix ] soft/rpib_gpu_firmware/do_build.sh invalid destination directory

--- soft/rpib_gpu_firmware/do_build.sh
+++ soft/rpib_gpu_firmware/do_build.sh
@@ -3,4 +3,6 @@ if [ $LFS_DO_BUILD_BYHAND == 0 ]; then
  return
 fi
 
-cp -rf $LFS_THIS_SOFT_SRC/* $LFS_TARGET_INSTALL_DIR/
+boot_dir=$LFS_TARGET_INSTALL_DIR/boot
+mkdir -p $boot_dir
+cp -rf $LFS_THIS_SOFT_SRC/* $boot_dir/
