8fdd38ae1b04ff83f6f88af3a0df8503c539ac75
lementec <lementec@esrf.fr>
2013-02-08 10:08:24 +0100

    [ update ] sh/do_user_globals.sh

--- sh/do_user_globals.sh
+++ sh/do_user_globals.sh
@@ -1,10 +1,10 @@
 case "$HOSTNAME" in
  'PCLAB245')
   # proxy settings
-  export http_proxy=http://proxy.esrf.fr:3128
-  export ftp_proxy=ftp://proxy.esrf.fr:3128
-  export all_proxy=socks://proxy.esrf.fr:3128
-  export https_proxy=https://proxy.esrf.fr:3128
+  export http_proxy=http://proxy.esrf.fr:3128/
+  export ftp_proxy=ftp://proxy.esrf.fr:3128/
+  export all_proxy=socks://proxy.esrf.fr:3128/
+  export https_proxy=https://proxy.esrf.fr:3128/
   # toolchains
   export LFS_CROSS_COMPILE_x86=/home/lementec/segfs/repo/dance/local/cross/.build/i686-nptl-linux-gnu/bin/i686-nptl-linux-gnu-
   export LFS_CROSS_COMPILE_armv6=''
