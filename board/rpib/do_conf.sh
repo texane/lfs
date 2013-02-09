35b8810941c6d9d21d36509e27fb5bc108393c93
Fabien Le Mentec <texane@gmail.com>
2013-02-09 12:31:02 -0600

    [ fix ] board/rpib use real kernel version

--- board/rpib/do_conf.sh
+++ board/rpib/do_conf.sh
@@ -1,7 +1,15 @@
+# disk partition sizes
+export LFS_DISK_EMPTY_SIZE=0
+export LFS_DISK_BOOT_SIZE=128
+export LFS_DISK_ROOT_SIZE=128
+export LFS_DISK_SIZE=$(($LFS_DISK_EMPTY_SIZE + $LFS_DISK_BOOT_SIZE + $LFS_DISK_ROOT_SIZE))
+
+# archi
 export LFS_TARGET_ARCH=arm
 export LFS_CROSS_COMPILE=$LFS_CROSS_COMPILE_armv6
 
-export LFS_LINUX_VERS=3.6.y
+# softwares
+export LFS_LINUX_VERS=3.6.11
 export LFS_LINUX_ARCH=arm
 
 . $LFS_TOP_DIR/board/common/do_conf.sh
