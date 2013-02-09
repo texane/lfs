efcabde19acc3faa34ee7f6c44614a56877af186
Fabien Le Mentec <texane@gmail.com>
2013-02-09 16:33:49 -0600

    [ update ] soft/cross_sysroot, chmod after install

--- soft/cross_sysroot/do_build_byhand.sh
+++ soft/cross_sysroot/do_build_byhand.sh
@@ -2,3 +2,4 @@ basename=`basename $LFS_CROSS_COMPILE`
 dirname=`dirname $LFS_CROSS_COMPILE`
 sysroot_dir=$dirname/../${basename%?}/sysroot
 cp -rf $sysroot_dir/* $LFS_TARGET_INSTALL_DIR/
+chmod -R 755 $LFS_TARGET_INSTALL_DIR
