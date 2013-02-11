83e99a2faa72cce1df73904f96c341aa28c56d65
lementec <lementec@esrf.fr>
2013-02-11 13:20:35 +0100

    [ fix ] soft/grub, use --modules=part_msdos to fix no such device error

--- soft/grub/do_post_build.sh
+++ soft/grub/do_post_build.sh
@@ -2,8 +2,9 @@ boot_dir=$LFS_TARGET_INSTALL_DIR/boot
 
 sudo \
 $LFS_HOST_INSTALL_DIR/sbin/grub-install \
+--modules=part_msdos \
 --boot-directory=$boot_dir \
 $LFS_DISK_DEV
 
-cp $LFS_THIS_SOFT_DIR/grub.cfg $boot_dir/grub/
-sed -i -e "s/LFS_LINUX_VERS/$LFS_LINUX_VERS/g" $boot_dir/grub/grub.cfg
+cp $LFS_THIS_SOFT_DIR/grub.cfg $boot_dir/grub/grub.cfg
+sed -i -e "s/LFS_LINUX_VERS/$LFS_LINUX_VERS/g" $boot_dir/grub/grub.cfg > /dev/null 2>&1
