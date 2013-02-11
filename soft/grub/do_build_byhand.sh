504bc3246110144cd24c708debd4b1c294ef310b
lementec <lementec@esrf.fr>
2013-02-11 10:24:26 +0100

    [ fix ] soft/grub, invalid grub.cfg local path

--- soft/grub/do_build_byhand.sh
+++ soft/grub/do_build_byhand.sh
@@ -5,5 +5,5 @@ $LFS_HOST_INSTALL_DIR/sbin/grub-install \
 --boot-directory=$boot_dir \
 $LFS_DISK_DEV
 
-cp $LFS_THIS_SOFT_DIR/grub/grub.cfg $boot_dir/grub/
+cp $LFS_THIS_SOFT_DIR/grub.cfg $boot_dir/grub/
 sed -i -e "s/LFS_LINUX_VERS/$LFS_LINUX_VERS/g" $boot_dir/grub/grub.cfg
