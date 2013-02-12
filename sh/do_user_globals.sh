468b2c83fb8b13fa4e1bdc5afa583d9457fe9e85
lementec <lementec@esrf.fr>
2013-02-12 15:53:28 +0100

    [ update ] default toolchain paths

--- sh/do_user_globals.sh
+++ sh/do_user_globals.sh
@@ -1,3 +1,7 @@
+# toolchains
+export LFS_CROSS_COMPILE_x86=$LFS_HOST_INSTALL_DIR/i686-nptl-linux-gnu/bin/i686-nptl-linux-gnu-
+export LFS_CROSS_COMPILE_armv6=$LFS_HOST_INSTALL_DIR/armv6-rpi-linux-gnueabi/bin/armv6-rpi-linux-gnueabi-
+
 case "$HOSTNAME" in
  'PCLAB245')
   # proxy settings
@@ -5,12 +9,5 @@ case "$HOSTNAME" in
   export https_proxy=https://proxy.esrf.fr:3128
   # DONT_USE: ftp_proxy=ftp://proxy.esrf.fr:3128
   # DONT_USE: all_proxy=socks://proxy.esrf.fr:3128
-  # toolchains
-  export LFS_CROSS_COMPILE_x86=$LFS_HOST_INSTALL_DIR/i686-nptl-linux-gnu/bin/i686-nptl-linux-gnu-
-  export LFS_CROSS_COMPILE_armv6=''
-  ;;
- 'dell')
-  export LFS_CROSS_COMPILE_x86=''
-  export LFS_CROSS_COMPILE_armv6=$LFS_HOST_INSTALL_DIR/armv6-rpi-linux-gnueabi/bin/armv6-rpi-linux-gnueabi-
   ;;
 esac
