03cfb8cdab4cae1bad3f29ecca40e79d0d42f2d5
lementec <lementec@esrf.fr>
2013-02-12 12:53:10 +0100

    [ update, add ] soft/crosstool-ng, gcc-4.6.0 patch

--- soft/crosstool-ng/do_build_byhand.sh
+++ soft/crosstool-ng/do_build_byhand.sh
@@ -30,5 +30,15 @@ sed -i -e "s/^\($key\)=\"\(.*\)\"$/\1=\"$val\"/g" $file_path
 vers=${LFS_LINUX_VERS//./_}
 sed -i -e "s/^CT_KERNEL_V_\(.*\)=y$/CT_KERNEL_V_$vers=y/g" $file_path
 
-# build a toolchain
+# some components (ie. gcc-4.6.0) have to be patched. we stop the build
+# after tarballs have been extracted, apply any required patch, then
+# restart the build.
+
+$LFS_HOST_INSTALL_DIR/bin/ct-ng build STOP=libc_check_config
+
+if [ -d $LFS_THIS_SOFT_SRC/.build/src/gcc-4.6.0 ]; then
+ < $LFS_THIS_SOFT_DIR/gcc-4.6.0.diff \
+ patch -p1 -d $LFS_THIS_SOFT_SRC/.build/src/gcc-4.6.0
+fi
+
 $LFS_HOST_INSTALL_DIR/bin/ct-ng build
