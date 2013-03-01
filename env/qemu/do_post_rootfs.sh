3cee918f58ea3fbec4acadad6402470b95442f37
fabien le mentec <lementec@esrf.fr>
2013-03-01 11:27:54 -0600

    [ update ] env/qemu, install local libs

--- env/qemu/do_post_rootfs.sh
+++ env/qemu/do_post_rootfs.sh
@@ -12,3 +12,7 @@ chmod 755 $LFS_TARGET_INSTALL_DIR/etc/init.d/10_dropbear
 
 cp $this_dir/grub.cfg $boot_dir/grub/grub.cfg
 sed -i -e "s/LFS_LINUX_VERS/$LFS_LINUX_VERS/g" $boot_dir/grub/grub.cfg > /dev/null 2>&1
+
+# no cross compile, copy local libs
+cp -rf /lib/x86_64-linux-gnu $LFS_TARGET_INSTALL_DIR/lib/
+cp -rf /lib64 $LFS_TARGET_INSTALL_DIR/
