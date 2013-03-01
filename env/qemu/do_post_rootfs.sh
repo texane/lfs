461a17deb0d1328449dcf443f0c12a14aa3a761a
fabien le mentec <lementec@esrf.fr>
2013-03-01 11:18:59 -0600

    [ update, add ] qemu/grub.cfg

--- env/qemu/do_post_rootfs.sh
+++ env/qemu/do_post_rootfs.sh
@@ -1,4 +1,5 @@
 this_dir=$LFS_TOP_DIR/env/$LFS_THIS_ENV_NAME
+boot_dir=$LFS_TARGET_INSTALL_DIR/boot
 
 cp $this_dir/interface.eth0 $LFS_TARGET_INSTALL_DIR/etc/network.d/
 cp $this_dir/network.conf $LFS_TARGET_INSTALL_DIR/etc/
@@ -8,3 +9,6 @@ cp $this_dir/shadow $LFS_TARGET_INSTALL_DIR/etc/
 
 cp $this_dir/10_dropbear $LFS_TARGET_INSTALL_DIR/etc/init.d
 chmod 755 $LFS_TARGET_INSTALL_DIR/etc/init.d/10_dropbear
+
+cp $this_dir/grub.cfg $boot_dir/grub/grub.cfg
+sed -i -e "s/LFS_LINUX_VERS/$LFS_LINUX_VERS/g" $boot_dir/grub/grub.cfg > /dev/null 2>&1
