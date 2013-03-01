7685ef909d511d4f4be73273a5a410e0a2cc2945
fabien le mentec <lementec@esrf.fr>
2013-03-01 07:37:04 -0600

    [ update ] proxy settings, linux tarball url if not rpib

--- sh/do_user_globals.sh
+++ sh/do_user_globals.sh
@@ -1,5 +1,6 @@
 # toolchains
 export LFS_CROSS_COMPILE_x86=$LFS_HOST_INSTALL_DIR/i686-nptl-linux-gnu/bin/i686-nptl-linux-gnu-
+export LFS_CROSS_COMPILE_x86_64=/usr/bin/
 export LFS_CROSS_COMPILE_armv6=$LFS_HOST_INSTALL_DIR/armv6-rpi-linux-gnueabi/bin/armv6-rpi-linux-gnueabi-
 
 case "$HOSTNAME" in
@@ -9,5 +10,9 @@ case "$HOSTNAME" in
   export https_proxy=https://proxy.esrf.fr:3128
   # DONT_USE: ftp_proxy=ftp://proxy.esrf.fr:3128
   # DONT_USE: all_proxy=socks://proxy.esrf.fr:3128
+  ;;
+ 'pclab245bis')
+  export http_proxy=http://proxy.esrf.fr:3128
+  export https_proxy=https://proxy.esrf.fr:3128
   ;;
 esac
