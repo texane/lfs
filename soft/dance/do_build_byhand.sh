c2e0dd95f81ab8e0bbb2b7b87b8dde23878e8073
lementec <lementec@esrf.fr>
2013-04-03 18:00:09 +0200

    [ update ] dance package installed based on board

--- soft/dance/do_build_byhand.sh
+++ soft/dance/do_build_byhand.sh
@@ -8,6 +8,7 @@ cd $LFS_THIS_SOFT_SRC/linuxcore/driver/pcidrvgen/src
 CROSS_COMPILE=$LFS_CROSS_COMPILE make comex
 cp linux-$LFS_LINUX_VERS/pcidrvgen.ko $LFS_TARGET_INSTALL_DIR/lib/modules/$LFS_LINUX_VERS/dance/pcidrvgen.ko
 
-cd $LFS_THIS_SOFT_SRC/linuxcore/libepci
-CROSS_COMPILE=$LFS_CROSS_COMPILE make comex
-cp linux-$LFS_LINUX_VERS/pcidrvgen.ko $LFS_TARGET_INSTALL_DIR/lib/modules/$LFS_LINUX_VERS/dance/pcidrvgen.ko
+cd $LFS_THIS_SOFT_SRC/linuxcore/libepci/src
+CROSS_COMPILE=$LFS_CROSS_COMPILE make
+mkdir -p $LFS_TARGET_INSTALL_DIR/usr/lib
+cp libepci.so $LFS_TARGET_INSTALL_DIR/usr/lib/libepci.so
--- soft/dance/do_conf.sh
+++ soft/dance/do_conf.sh
@@ -1,4 +1,4 @@
-[ -z $LFS_DANCE_VERS ] && return
+[ $LFS_THIS_BOARD_NAME != 'comex' ] && return
 
 LFS_THIS_SOFT_IS_ENABLED=1
 LFS_THIS_SOFT_URL=svn://https://gaia.esrf.fr/svn/ISDDcommon/Projects/Dance
