f25faf7c8341c7becc37fc153b8ffb48ee0ed43c
lementec <lementec@esrf.fr>
2013-04-03 17:53:01 +0200

    [ update ] pcidrvgen now part of the DANCE package

--- soft/dance/do_build_byhand.sh
+++ soft/dance/do_build_byhand.sh
@@ -7,3 +7,7 @@ cp linux-$LFS_LINUX_VERS/epcidrv.ko $LFS_TARGET_INSTALL_DIR/lib/modules/$LFS_LIN
 cd $LFS_THIS_SOFT_SRC/linuxcore/driver/pcidrvgen/src
 CROSS_COMPILE=$LFS_CROSS_COMPILE make comex
 cp linux-$LFS_LINUX_VERS/pcidrvgen.ko $LFS_TARGET_INSTALL_DIR/lib/modules/$LFS_LINUX_VERS/dance/pcidrvgen.ko
+
+cd $LFS_THIS_SOFT_SRC/linuxcore/libepci
+CROSS_COMPILE=$LFS_CROSS_COMPILE make comex
+cp linux-$LFS_LINUX_VERS/pcidrvgen.ko $LFS_TARGET_INSTALL_DIR/lib/modules/$LFS_LINUX_VERS/dance/pcidrvgen.ko
--- soft/pcidrvgen/do_conf.sh
+++ soft/pcidrvgen/do_conf.sh
@@ -1,10 +1,12 @@
-[ $LFS_THIS_BOARD_NAME != 'comex' ] && return
+# disabled, now shipped with the dance package
 
-LFS_THIS_SOFT_IS_ENABLED=1
-LFS_THIS_SOFT_DEPS='linux'
-LFS_THIS_SOFT_URL=file://$LFS_THIS_SOFT_DIR/pcidrvgen.tar.gz
-LFS_THIS_SOFT_BUILD_METHOD=make
+# [ $LFS_THIS_BOARD_NAME != 'comex' ] && return
 
-LFS_THIS_SOFT_MAKE_ARGS=\
-" KDIR=$LFS_SRC_DIR/linux"\
-" MODLIB=$LFS_TARGET_INSTALL_DIR/lib/modules/$LFS_LINUX_VERS"
+# LFS_THIS_SOFT_IS_ENABLED=1
+# LFS_THIS_SOFT_DEPS='linux'
+# LFS_THIS_SOFT_URL=file://$LFS_THIS_SOFT_DIR/pcidrvgen.tar.gz
+# LFS_THIS_SOFT_BUILD_METHOD=make
+
+# LFS_THIS_SOFT_MAKE_ARGS=\
+# " KDIR=$LFS_SRC_DIR/linux"\
+# " MODLIB=$LFS_TARGET_INSTALL_DIR/lib/modules/$LFS_LINUX_VERS"
