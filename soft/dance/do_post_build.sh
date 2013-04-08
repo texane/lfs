d693e1e9bea295f36b8907487ef6266dd7e5e575
lementec <lementec@esrf.fr>
2013-04-08 17:13:00 +0200

    [ rename ] soft/dance init.d scripts

--- soft/dance/do_post_build.sh
+++ soft/dance/do_post_build.sh
@@ -1,7 +1,7 @@
 # install init scripts
 initd_dir=$LFS_TARGET_INSTALL_DIR/etc/init.d
 [ -d $initd_dir ] || mkdir -p $initd_dir
-cp $LFS_THIS_SOFT_DIR/20_dance $initd_dir
+cp $LFS_THIS_SOFT_DIR/etc/init.d/20_dance $initd_dir
 chmod 755 $initd_dir/20_dance
 
 # install dance profile
