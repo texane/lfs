10a77c616fab2a7480f1f20e816b3f80afdcaa83
lementec <lementec@esrf.fr>
2013-04-08 16:04:33 +0200

    [ update ] soft/dance, minor

--- soft/dance/do_build_byhand.sh
+++ soft/dance/do_build_byhand.sh
@@ -1,3 +1,13 @@
+# compile install libepci
+
+cd $LFS_THIS_SOFT_SRC/linuxcore/libepci/src
+CROSS_COMPILE=$LFS_CROSS_COMPILE make
+mkdir -p $LFS_TARGET_INSTALL_DIR/lib/dance
+cp libepci.so $LFS_TARGET_INSTALL_DIR/lib/dance/libepci.so
+
+
+# compile install pcidrvgen and epcidrv modules
+
 mkdir -p $LFS_TARGET_INSTALL_DIR/lib/modules/$LFS_LINUX_VERS/dance
 
 cd $LFS_THIS_SOFT_SRC/linuxcore/driver/epcidrv/src
@@ -7,8 +17,3 @@ cp linux-$LFS_LINUX_VERS/epcidrv.ko $LFS_TARGET_INSTALL_DIR/lib/modules/$LFS_LIN
 cd $LFS_THIS_SOFT_SRC/linuxcore/driver/pcidrvgen/src
 CROSS_COMPILE=$LFS_CROSS_COMPILE make comex
 cp linux-$LFS_LINUX_VERS/pcidrvgen.ko $LFS_TARGET_INSTALL_DIR/lib/modules/$LFS_LINUX_VERS/dance/pcidrvgen.ko
-
-cd $LFS_THIS_SOFT_SRC/linuxcore/libepci/src
-CROSS_COMPILE=$LFS_CROSS_COMPILE make
-mkdir -p $LFS_TARGET_INSTALL_DIR/usr/lib
-cp libepci.so $LFS_TARGET_INSTALL_DIR/usr/lib/libepci.so
