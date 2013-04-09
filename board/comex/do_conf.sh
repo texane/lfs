caea02469d9f446db2ebc9cf570062b728153b1c
lementec <lementec@esrf.fr>
2013-04-09 13:47:33 +0200

    [ add ] soft/ntpd

--- board/comex/do_conf.sh
+++ board/comex/do_conf.sh
@@ -15,5 +15,6 @@ export LFS_LINUX_VERS=3.6.11
 export LFS_LINUX_ARCH=x86
 export LFS_PCIUTILS_VERS=3.1.10
 export LFS_GRUB_VERS=2.00
+export LFS_NTPD_VERS=1.0
 
 . $LFS_TOP_DIR/board/common/do_conf.sh
---
+++ soft/ntpd/do_post_build.sh
@@ -0,0 +1,4 @@
+initd_dir=$LFS_TARGET_INSTALL_DIR/etc/init.d
+[ -d $initd_dir ] || mkdir -p $initd_dir
+cp $LFS_THIS_SOFT_DIR/10_ntpd $initd_dir
+chmod 755 $initd_dir/10_ntpd
