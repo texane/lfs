27d12ef322953647aa625773496d35098996efb1
lementec <lementec@esrf.fr>
2013-04-09 18:35:13 +0200

    [ add ] soft/ntpd, localtime

--- soft/dance/do_post_build.sh
+++ soft/dance/do_post_build.sh
@@ -7,4 +7,4 @@ chmod 755 $initd_dir/20_dance
 # install dance profile
 [ -d $LFS_TARGET_INSTALL_DIR/etc/dance ] || mkdir $LFS_TARGET_INSTALL_DIR/etc/dance
 cp $LFS_THIS_SOFT_DIR/etc/dance/profile $LFS_TARGET_INSTALL_DIR/etc/dance/profile
-chmod 755 -R $LFS_TARGET_INSTALL_DIR/etc/dance/profile
+chmod 755 $LFS_TARGET_INSTALL_DIR/etc/dance/profile
--- soft/ntpd/do_post_build.sh
+++ soft/ntpd/do_post_build.sh
@@ -2,3 +2,6 @@ initd_dir=$LFS_TARGET_INSTALL_DIR/etc/init.d
 [ -d $initd_dir ] || mkdir -p $initd_dir
 cp $LFS_THIS_SOFT_DIR/10_ntpd $initd_dir
 chmod 755 $initd_dir/10_ntpd
+
+cp $LFS_THIS_SOFT_DIR/localtime $LFS_TARGET_INSTALL_DIR/etc/localtime
+chmod 755 $LFS_TARGET_INSTALL_DIR/etc/localtime
