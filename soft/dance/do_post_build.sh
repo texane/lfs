60f26741c92c54c76fc12a47c7e6cb346a8e80ea
lementec <lementec@esrf.fr>
2013-04-09 17:51:48 +0200

    [ update ] soft/busybox, ntpd option

--- soft/dance/do_post_build.sh
+++ soft/dance/do_post_build.sh
@@ -5,5 +5,6 @@ cp $LFS_THIS_SOFT_DIR/etc/init.d/20_dance $initd_dir
 chmod 755 $initd_dir/20_dance
 
 # install dance profile
+[ -d $LFS_TARGET_INSTALL_DIR/etc/dance ] || mkdir $LFS_TARGET_INSTALL_DIR/etc/dance
 cp $LFS_THIS_SOFT_DIR/etc/dance/profile $LFS_TARGET_INSTALL_DIR/etc/dance/profile
 chmod 755 -R $LFS_TARGET_INSTALL_DIR/etc/dance/profile
