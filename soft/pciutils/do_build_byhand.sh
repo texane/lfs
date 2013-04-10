06deb78a189945edce3faf376b503124e6b02dde
lementec <lementec@esrf.fr>
2013-04-10 12:57:00 +0200

    [ update ] soft/pciutils, enable static version too

--- soft/pciutils/do_build_byhand.sh
+++ soft/pciutils/do_build_byhand.sh
@@ -1,5 +1,7 @@
 cd $LFS_THIS_SOFT_SRC
 
+# make both static and shared libs
+
 make install \
 CROSS_COMPILE=$LFS_CROSS_COMPILE \
 PREFIX=$LFS_TARGET_INSTALL_DIR \
@@ -11,6 +13,20 @@ make install-lib \
 CROSS_COMPILE=$LFS_CROSS_COMPILE \
 PREFIX=$LFS_TARGET_INSTALL_DIR \
 SHARED=yes \
+ZLIB=no \
+DNS=no
+
+make clean install \
+CROSS_COMPILE=$LFS_CROSS_COMPILE \
+PREFIX=$LFS_TARGET_INSTALL_DIR \
+SHARED=no \
+ZLIB=no \
+DNS=no
+
+make install-lib \
+CROSS_COMPILE=$LFS_CROSS_COMPILE \
+PREFIX=$LFS_TARGET_INSTALL_DIR \
+SHARED=no \
 ZLIB=no \
 DNS=no
 
