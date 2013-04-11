200b1d7db09ffcc51b31b2527ab401df9f87a785
lementec <lementec@esrf.fr>
2013-04-11 21:40:35 +0200

    [ update ] use sdk toolchain

--- sh/do_user_globals.sh
+++ sh/do_user_globals.sh
@@ -10,9 +10,11 @@ case "$HOSTNAME" in
   export https_proxy=https://proxy.esrf.fr:3128
   # DONT_USE: ftp_proxy=ftp://proxy.esrf.fr:3128
   # DONT_USE: all_proxy=socks://proxy.esrf.fr:3128
+  export LFS_CROSS_COMPILE_x86=/segfs/linux/dance_sdk/toolchain/i686-nptl-linux-gnu/bin/i686-nptl-linux-gnu-
   ;;
  'pclab245bis')
   export http_proxy=http://proxy.esrf.fr:3128
   export https_proxy=https://proxy.esrf.fr:3128
+  export LFS_CROSS_COMPILE_x86=/segfs/linux/dance_sdk/toolchain/i686-nptl-linux-gnu/bin/i686-nptl-linux-gnu-
   ;;
 esac
