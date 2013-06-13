991ba96891fa2e7972ac3dfae2dfdd07df0c5955
lementec <lementec@esrf.fr>
2013-06-13 11:24:34 +0200

    [ update, add ] dance profile.local and profile.segfs provided. profile links to profile.local by default.

--- soft/dance/do_post_build.sh
+++ soft/dance/do_post_build.sh
@@ -6,5 +6,8 @@ chmod 755 $initd_dir/20_dance
 
 # install dance profile
 [ -d $LFS_TARGET_INSTALL_DIR/etc/dance ] || mkdir $LFS_TARGET_INSTALL_DIR/etc/dance
-cp $LFS_THIS_SOFT_DIR/etc/dance/profile $LFS_TARGET_INSTALL_DIR/etc/dance/profile
-chmod 755 $LFS_TARGET_INSTALL_DIR/etc/dance/profile
+cp $LFS_THIS_SOFT_DIR/etc/dance/profile.local $LFS_TARGET_INSTALL_DIR/etc/dance/profile.local
+chmod 755 $LFS_TARGET_INSTALL_DIR/etc/dance/profile.local
+cp $LFS_THIS_SOFT_DIR/etc/dance/profile.segfs $LFS_TARGET_INSTALL_DIR/etc/dance/profile.segfs
+chmod 755 $LFS_TARGET_INSTALL_DIR/etc/dance/profile.segfs
+ln -s profile.local $LFS_TARGET_INSTALL_DIR/etc/dance/profile
