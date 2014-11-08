77906a8a6259f37f0e0ae4bc5d28b6fa8778891b
texane <texane@gmail.com>
2014-11-07 21:45:53 -0600

    [ add ] tools/mount_rootfs

---
+++ tools/mount_rootfs/main.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+[ -z $1 ] && ( echo 'missing disk path'; exit -1; )
+[ -z $2 ] && ( echo 'missing mount path'; exit -1; )
+
+disk_path=$1
+mount_path=$2
+
+# format: name start end count
+line=(`/sbin/fdisk -l $disk_path | grep Linux`)
+off=$((${line[1]} * 512))
+size=$((${line[2]} * 512))
+
+loop_path=`sudo losetup --show -f --offset=$off --sizelimit=$size $disk_path`
+[ $? = 0 ] || ( echo 'losetup failed'; exit -1; )
+
+sudo mount $loop_path $mount_path
+[ $? = 0 ] || ( echo 'mount failed'; sudo losetup -d $loop_path; exit -1; )
+
+# on success, caller must umount and losetup -d when done
