f9c22cf339bd83004024f27bfaca61b58a07f9b2
lementec <lementec@esrf.fr>
2014-11-13 18:51:32 +0100

    [ update ] escape string before sed

--- soft/uboot/do_conf.sh
+++ soft/uboot/do_conf.sh
@@ -3,5 +3,5 @@ is_enabled='no'
 [ is_enabled == 'no' ] && return
 
 LFS_THIS_SOFT_IS_ENABLED=1
-LFS_THIS_SOFT_URL=file:///segfs/linux/dance_sdk/tarballs/seco_imx6/uboot.tar.gz ;;
+LFS_THIS_SOFT_URL=file:///segfs/linux/dance_sdk/tarballs/seco_imx6/uboot.tar.gz
 LFS_THIS_SOFT_BUILD_METHOD=byhand
