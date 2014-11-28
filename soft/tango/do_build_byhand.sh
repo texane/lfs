deb3ad4753c8c285595399746269b8fe68388237
lementec <lementec@esrf.fr>
2014-11-28 10:33:22 +0100

    [ add ] tango

---
+++ soft/tango/do_build_byhand.sh
@@ -0,0 +1,50 @@
+cd $LFS_THIS_SOFT_SRC
+
+
+# build zeromq
+
+cd $LFS_THIS_SOFT_SRC/zeromq
+
+CC=$LFS_CROSS_COMPILE\gcc \
+CXX=$LFS_CROSS_COMPILE\g++ \
+./configure \
+--prefix=$LFS_TARGET_INSTALL_DIR \
+--host=$LFS_TARGET_ARCH \
+--build=x86_64-linux
+
+make && make install
+
+
+# build omnirb
+
+cd $LFS_THIS_SOFT_SRC/omniORB
+
+CC=$LFS_CROSS_COMPILE\gcc \
+CXX=$LFS_CROSS_COMPILE\g++ \
+./configure \
+--prefix=$LFS_TARGET_INSTALL_DIR \
+--host=$LFS_TARGET_ARCH \
+--build=x86_64-linux
+
+make CC=gcc -C src/tool/omniidl/cxx/cccp
+make CXX=g++ -C src/tool/omniidl/cxx
+make CC=gcc -C src/tool/omkdepend
+make && make install
+
+
+# build tango
+
+cd $LFS_THIS_SOFT_SRC/tango
+
+CC=$LFS_CROSS_COMPILE\gcc \
+CXX=$LFS_CROSS_COMPILE\g++ \
+./configure \
+--host=$LFS_TARGET_ARCH \
+--build=x86_64-linux \
+--prefix=$LFS_TARGET_INSTALL_DIR \
+--with-zlib=$LFS_TARGET_INSTALL_DIR \
+--with-omni=$LFS_TARGET_INSTALL_DIR \
+--with-zmq=$LFS_TARGET_INSTALL_DIR \
+--disable-java
+
+make && make install
---
+++ soft/tango/do_retrieve_byhand.sh
@@ -0,0 +1,58 @@
+# assume LFS_THIS_SOFT_TAR
+# assume LFS_THIS_SOFT_TAR_EXT
+
+function do_retrieve_extract {
+ tar_url=$1
+ tar_dir=$2
+
+ tar_name=`basename $tar_url`
+
+ case $tar_name in
+  *.tar)
+   ext='.tar'
+   opt='xvf'
+   ;;
+  *.tar.gz)
+   ext='.tar.gz'
+   opt='xzvf'
+   ;;
+  *.tgz)
+   ext='.tgz'
+   opt='xzvf'
+   ;;
+  *.tar.bz2)
+   ext='.tar.bz2'
+   opt='xjvf'
+   ;;
+ esac
+
+ tar_path=$tar_dir.$ext
+ wget $tar_url -O $tar_path
+ mkdir $tar_dir
+ tar $opt $tar_path --strip 1 -C $tar_dir
+ rm $tar_path
+}
+
+# remove trailing extension
+tmp_dir=${LFS_THIS_SOFT_TAR::-${#LFS_THIS_SOFT_TAR_EXT}}
+mkdir -p $tmp_dir
+
+# zeromq
+do_retrieve_extract \
+http://download.zeromq.org/zeromq-4.1.0-rc1.tar.gz \
+$tmp_dir/zeromq
+
+# omniorb
+do_retrieve_extract \
+http://optimate.dl.sourceforge.net/project/omniorb/omniORB/omniORB-4.1.6/omniORB-4.1.6.tar.bz2 \
+$tmp_dir/omniORB
+
+# tango
+do_retrieve_extract \
+http://cznic.dl.sourceforge.net/project/tango-cs/tango-8.1.2c.tar.gz \
+$tmp_dir/tango
+
+# make soft tarball
+cd `dirname $tmp_dir`
+tar czvf $LFS_THIS_SOFT_TAR `basename $tmp_dir`
+rm -rf $tmp_dir
