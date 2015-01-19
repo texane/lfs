70a62ef978c90df30ef9220130860d4b4d326266
lementec <lementec@esrf.fr>
2015-01-19 16:43:40 +0100

    do not copy static libs

--- soft/cross_sysroot/do_build_byhand.sh
+++ soft/cross_sysroot/do_build_byhand.sh
@@ -1,5 +1,18 @@
 basename=`basename $LFS_CROSS_COMPILE`
 dirname=`dirname $LFS_CROSS_COMPILE`
 sysroot_dir=$dirname/../${basename%?}/sysroot
-cp -rf $sysroot_dir/* $LFS_TARGET_INSTALL_DIR/
+
+# reduce system size by removing .la and .a files
+n=${#sysroot_dir}
+l=`find $sysroot_dir | grep -v '.a$' | grep -v '.la$'`
+for s in $l; do
+ d=$LFS_TARGET_INSTALL_DIR/${s:n}
+ mkdir -p `dirname $d` 2> /dev/null
+ if [ -d $d ]; then
+  mkdir -p $d 2> /dev/null
+ else
+  cp -P $s $d 2> /dev/null
+ fi
+done
+
 chmod -f -R 755 $LFS_TARGET_INSTALL_DIR
