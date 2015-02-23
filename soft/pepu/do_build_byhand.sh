0c440e724e72cbec814109e30eb34fe27907e0e8
lementec <lementec@esrf.fr>
2015-02-23 12:44:36 +0100

    add soft/pepu

---
+++ soft/pepu/do_build_byhand.sh
@@ -0,0 +1,17 @@
+# select dance_sdk_platform
+# CROSS_COMPILE is set by the build environment
+
+case $LFS_THIS_BOARD_NAME in
+ comex|seco_x86) dance_sdk_platform='kontron_type10' ;;
+ conga_imx6) dance_sdk_platform='conga_imx6' ;;
+ seco_imx6) dance_sdk_platform='seco_imx6' ;;
+ seco_uimx6) dance_sdk_platform='seco_uimx6' ;;
+ *) dance_sdk_platform='local' ;;
+esac
+
+d=$LFS_TARGET_INSTALL_DIR/app/bin/dance
+mkdir -p $d
+
+cd $LFS_THIS_SOFT_SRC/instruments/pepu/firmware/controller
+make DANCE_SDK_PLATFORM=$dance_sdk_platform DANCE_SDK_DEV_DIR=$LFS_THIS_SOFT_SRC/components devel
+cp $LFS_THIS_SOFT_SRC/instruments/pepu/firmware/controller/pepu $d/main
---
+++ soft/pepu/do_retrieve_byhand.sh
@@ -0,0 +1,20 @@
+# assume LFS_THIS_SOFT_TAR
+# assume LFS_THIS_SOFT_TAR_EXT
+
+# remove trailing extension
+tmp_path=${LFS_THIS_SOFT_TAR::-${#LFS_THIS_SOFT_TAR_EXT}}
+mkdir -p $tmp_path
+
+repo=https://deg-svn.esrf.fr/svn
+
+compos='libepci libedma libebuf libefpak libpmem libuirq libdance'
+for c in $compos; do
+ url=$repo/$c/dev
+ svn export $url $tmp_path/components/$c ;
+done
+
+svn export $repo/pepu/dev $tmp_path/instruments/pepu ;
+
+cd `dirname $tmp_path`
+tar czvf $LFS_THIS_SOFT_TAR `basename $tmp_path`
+rm -rf $tmp_path
