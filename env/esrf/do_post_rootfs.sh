466dceb7c746ccff1543b3f2bcf86b0fe997f007
fabien.lementec <fabien.lementec@gmail.com>
2016-10-05 15:28:27 +0200

    add env/esrf/udhcpc.conf

--- env/esrf/do_post_rootfs.sh
+++ env/esrf/do_post_rootfs.sh
@@ -3,6 +3,7 @@ this_dir=$LFS_TOP_DIR/env/$LFS_THIS_ENV_NAME
 cp $this_dir/interface.eth0 $LFS_TARGET_INSTALL_DIR/etc/network.d/
 cp $this_dir/network.conf $LFS_TARGET_INSTALL_DIR/etc/
 cp $this_dir/resolv.conf $LFS_TARGET_INSTALL_DIR/etc/
+cp $this_dir/udhcpc.conf $LFS_TARGET_INSTALL_DIR/etc/
 
 cp $this_dir/10_segfs $LFS_TARGET_INSTALL_DIR/etc/init.d
 mkdir -p $LFS_TARGET_INSTALL_DIR/segfs
